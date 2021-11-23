589fde76d5c4f4b428f8db02ac78c2e9ae5cab15
System Administrator <jwright12@winona.edu>
2021-11-22 21:27:17 -0600

    Print festival names on homepage

--- db-connect.php
+++ db-connect.php
@@ -1,13 +1,3 @@
 <?php
-$dbhost = $_SERVER['RDS_HOSTNAME'];
-$dbport = $_SERVER['RDS_PORT'];
-$dbname = 'TPCC';
-echo $dbname;
-$charset = 'utf8' ;
-
-$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-$username = $_SERVER['RDS_USERNAME'];
-$password = $_SERVER['RDS_PASSWORD'];
-
-$pdo = new PDO($dsn, $username, $password);
+$pdo = new PDO("mysql:host=localhost;dbname=music_festival", 'root', '');
 ?>
