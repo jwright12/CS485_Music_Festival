3db709fb9e93d176d0854b89262dea2e98f2eb30
Syd <49962283+honokarose@users.noreply.github.com>
2021-11-28 22:26:27 -0800

    admin only

---
+++ admin/admin.php
@@ -0,0 +1,6 @@
+<?php
+
+echo $i1 = "admin main page";
+echo $i2 = "should be what we see in the locid chart of admin.php";
+
+?>
---
+++ admin/manage_artist.php
@@ -0,0 +1,105 @@
+<?php
+	if(count($_POST)>0) {
+		include 'db-connect.php';
+		$pdo->beginTransaction();
+
+		$f = $_POST['first_name'];
+		$l = $_POST['last_name'];
+		$u = $_POST['username'];
+		$p = $_POST['password'];
+		$em = $_POST['email'];
+		$ph = $_POST['number'];
+		$di = $_POST['artist_id'];
+
+        
+
+		$query = $pdo->prepare("INSERT INTO user (user_id, f_name, l_name, username, user_password) values ('$di', '$f', '$l', '$u', '$p')");
+		$query->execute();
+
+        $query = $pdo->prepare("INSERT INTO artist (artist_id) values ('$di')");
+		$query->execute();
+
+		// Could be a problem. Make just use username as unique identifier
+		// Should probably also insert U ID into customer table
+		$query = $pdo->prepare("SELECT user.user_id FROM user WHERE user.f_name='$f' AND user.l_name='$l'");
+		$query->execute();
+
+		$array = $query->fetch();
+		$id = $array[0];
+
+		$query = $pdo->prepare("INSERT INTO user_email (user_id, email) values ('$id', '$em')");
+		$query->execute();
+
+		$query = $pdo->prepare("INSERT INTO user_phone (user_id, phone) values ('$id', '$ph')");
+		$query->execute();
+
+		$pdo->commit();
+
+		$message = "Artist Registration Successful";
+		
+		header("Location: http://localhost/artist.php?id=$id");
+	} 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Ultimate Music Festival Manager - Pro Version</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <style>body {margin-top: 40px; background-color: #333;}</style>
+        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="hero-unit">
+
+                <h1>Register New Artist</h1>
+				<?php if(isset($message)) { echo $message; } ?>
+				<br>
+				<form action="" method="post">
+
+					<div class="mb-3">
+							<label for="first_name" class="form-label"><b>First Name</b></label>
+							<input type="text" class="form-control" name="first_name">
+					</div>
+					<div class="mb-3">
+							<label for="last_name" class="form-label"><b>Last Name</b></label>
+							<input type="text" class="form-control" name="last_name">
+					</div>
+					<div class="mb-3">
+							<label for="email" class="form-label"><b>Email</b></label>
+							<input type="email" class="form-control" name="email">
+					</div>
+					<div class="mb-3">
+							<label for="username" class="form-label"><b>Username</b></label>
+							<input type="text" class="form-control" name="username">
+					</div>
+					<div class="mb-3">
+							<label for="password" class="form-label"><b>Password</b></label>
+							<input type="password" class="form-control" name="password">
+					</div>
+					<div class="mb-3">
+							<label for="number" class="form-label"><b>Phone Number</b></label>
+							<input type="text" class="form-control" name="number">
+					</div>
+                    <div class="mb-3">
+                            <label for="artist_id" class="form-label"><b>Artist ID</b></label>
+							<input type="int" class="form-control" name="artist_id">
+                    </div>
+
+					<button type="submit" class="btn btn-primary">Register</button>
+				</form>
+
+            </div> 
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+
+</html>
