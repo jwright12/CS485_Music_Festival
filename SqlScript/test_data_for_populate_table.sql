de4f5c2cc1b0d1df2bf69cdaba7f3741f53e2343
Syd <49962283+honokarose@users.noreply.github.com>
2021-11-28 22:32:18 -0800

    test data

---
+++ SqlScript/test_data_for_populate_table.sql
@@ -0,0 +1,25 @@
+insert into schedule values( 20001, 3, '2021-10-11 19:00:00');
+insert into schedule values( 30001, 4, '2021-10-11 19:00:00');
+insert into schedule values( 40001, 4, '2021-08-11 19:00:00');
+
+insert into ticket values (2000, 20, 500, 2000, 4, '2021-02-10 9:00:00');
+insert into ticket values (3000, 30, 500, 3000, 3, '2021-11-20 18:00:00');
+insert into ticket values (4000, 16, 500, 4000, 4, '2021-05-14 14:00:00');
+
+insert into festival values( 2000, 'WSU FUN', '2021-12-11 19:00:00', 'Gazibo', '20001', 3, '2021-10-11 19:00:00' );
+insert into festival values( 3000, 'COOL LOL', '2021-12-10 14:00:00', 'Gazibo', 30001, 4, '2021-09-11 19:00:00' );
+insert into festival values( 4000, 'NICE MAN', '2021-12-11 19:00:00', 'East Hall', 40001, 4, '2021-08-11 19:00:00' );
+
+
+insert into artist values ('80');
+insert into artist values ('81');
+insert into artist values ('82');
+
+insert into customer values ('90');
+insert into customer values ('91');
+insert into customer values ('92');
+
+insert into administrator values (9);
+insert into administrator values (99);
+insert into administrator values (999);
+insert into administrator values (666);
