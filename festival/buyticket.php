aecc3def78d8be01f7e197efb0a3ed29ac07ec08
Syd <49962283+honokarose@users.noreply.github.com>
2021-11-29 16:12:25 -0800

    Update buyticket.php

--- festival/buyticket.php
+++ festival/buyticket.php
@@ -1,24 +1,4 @@
-<?php
 
-if(isset($_POST['buyTicket'])) 
-{
-  $checkbox = isset($_POST['checkBuy']) ? $_POST['checkBuy'] : array();
-  $id = 0;
-  for($i=0;$i<count($checkbox);$i++)
-  {
-    $id = $checkbox[$i];
-    $deleteQuery = "update ticket set quant = quant-1 where ticket_id ='$id'";
-    $DeleteQueryExec = mysqli_query($conn, $deleteQuery);
-  }
-
-  echo '<script>alert("Thank you for purchasing");
-  window.location = "http://localhost/index.php";
-  </script>';
-
-
-
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,9 +17,11 @@ if(isset($_POST['buyTicket']))
             <div class="hero-unit">
 
                 <h1>Ticket Purchasing</h1>
-				<?php if(isset($message)) { echo $message; } ?>
+                
+				<?php 
+                if(isset($message)) { echo $message; } ?>
 				<br>
-				<form action="" method="post">
+				<form action="buyticket.php" method="post">
 
                 <script>
                     if ( window.history.replaceState ) {
@@ -48,6 +30,7 @@ if(isset($_POST['buyTicket']))
                 </script>
 
                 <?php
+                include '../db-connect.php';
                     if(isset($_POST['f_id']) && isset($_POST['id'])) {
                         include '../db-connect.php';
                         $fid = $_POST['f_id'];
@@ -58,25 +41,69 @@ if(isset($_POST['buyTicket']))
                                                 festival.festival_id,
                                                 festival.festival_date,
                                                 location,
-                                                ticket.ticket_id,
+                                                ticket.ticket_id as 'Ticket id',
                                                 ticket.price,
                                                 ticket.quant
                                                 FROM festival
                                                 join ticket
                                                 on festival.festival_id = ticket.festival_id
                                                 WHERE festival.festival_id = $fid");
+
                         $query->execute();
                         $array = $query->fetch();
+                        $row = $query->fetch();
                         $f_name = $array[0];
+                        $tid = $array[1];
+                        $date = $array[2];
+                        $locate = $array[3];
+                        $ticid = $array[4];
                         $price = $array[5];
                         $q = $array[6];
-                        echo "$f_name $price $q";
-                    }
-                ?>
 
-                Need a form to calculate the order details, insert to customer purchases, update ticket quant
-            </div> 
-        </div>
+                       // echo "$f_name $price $q";
+                       echo "<table width='98%' border='1'name='table';>
+                       <tr>
+                           <th>Festival</th>		
+                           <th>Location</th>
+                           <th>Date</th>
+                           <th>Price</th>
+                           <th>Seat left</th>
+                       </tr>
+                       <tr>
+                       <td>$f_name</td>
+                           <td>$locate</td>
+                           <td>$date</td>     
+                           <td>$price</td>
+                           <td>$q</td>
+                       </tr>";
+                       $query2 = $pdo->prepare("update ticket set ticket.quant = ticket.quant-1 where ticket.ticket_id ='$ticid'");
+                       $query2->execute();
+    
+             
+                    }
+
+?>
+					    <button name="buyit" type="submit" class="btn btn-primary">Buy Tickets</button>
+                        </form>
+
+
+                <?php
+ 
+
+                if(isset($_POST['buyit'])) 
+                    {
+
+                echo '<script>alert("Thank you for purchasing");
+                window.location = "http://localhost/index.php";
+                </script>';
+        
+                }
+ 
+ 
+ ?>
+
+            </div> <!-- hero-unit -->
+        </div> <!-- container -->
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
